5a7b62fe9c2353a6793c030d29161698141c9709
Ryder-CTL <bunnyjryder@gmail.com>
2026-05-20 15:17:12 -0700

    Add user prompts and bio logging to main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Write your code below
+var firstName = prompt (What is your first name?);
+var hometown = prompt (Where is your hometown?);
+var favoriteFood = prompt (What is your favorite food?);
+
+var bio = firstName + "is from" + hometown + "and their favorite food is" + favoriteFood + ".";
+
+console.log(bio);
